c3087eba2a0efad6a38e3d0988e7a80257b34552
Pacho: Francisco Nino <francisco.nino@experian.com>
2019-05-31 18:01:31 -0400

    Add information about last update

--- gitHealthCheck.py
+++ gitHealthCheck.py
@@ -1,7 +1,8 @@
 import requests
 import json
 from nested_lookup import nested_lookup
-from datetime import datetime
+#from datetime import datetime
+import datetime
 import argparse
 import sys
 import base64
@@ -45,30 +46,39 @@ try:
         response_jsondata = json.loads(response.content, encoding=None)
 
         branches = response_jsondata['values']
-        print(branches)
+        #print(branches)
         for branch in branches:
-            print(branch['displayId'])
+            print('    branch:' + branch['displayId'])
 
+            # Check Branch Name
             if branch['displayId'].upper() == 'MASTER':
                 print("        + You have a master ;) ")
-            elif branch['displayId'].upper() == 'DEVELOPMENT' or branch['displayId'].upper() == 'RELEASE':
+                if str(branch['isDefault']) == "True":
+                    print("        + master is your default branch ;) ")
+                else:
+                    print("        + master is NOT your default branch ;( ")
+            elif branch['displayId'].upper() == 'DEVELOPMENT' or branch['displayId'].upper() == 'RELEASE' or branch['displayId'].upper() == 'INTEGRATION':
                 print("        - Hummm... you shouldn't be using this ;( ")
             else:
                 pattern = 'feature/[A-Z]\w+-[0-9]\w+'
                 if re.match(pattern, branch['displayId']):
                     print("        - Nice branch name, i like it ;)")
                 else:
-                    print("        - Don't know if I like your branch name that much  ;(")              
+                    print("        - Don't know if I like your branch name that much  ;(")
             
-            #response = requests.get(args['baseurl'] + 'rest/api/1.0/projects/' + args['project'] + '/repos/' + repo + '/branches/' + branch['displayId'], headers=headers, params=(('limit', '1'),('details', 'true'),))
-            #print(response.content)
-            #authorTimestamp = nested_lookup('authorTimestamp', response_jsondata)
-            print(branch['metadata'])
-            #strDate = branch['authorTimestamp']/1000 # remove last 3 zeros from timestamp value
-            #print(strDate)
-            #[0:9]
+            #print(branch['metadata'])
+            #print(branch['metadata']['com.atlassian.bitbucket.server.bitbucket-branch:latest-commit-metadata']['authorTimestamp'])
+            strDate = branch['metadata']['com.atlassian.bitbucket.server.bitbucket-branch:latest-commit-metadata']['authorTimestamp']/1000 # remove last 3 zeros from timestamp value
             #dt_object = datetime.fromtimestamp(strDate)
-            #print('        authorTimestamp:' + str(dt_object))
+            dt_object = datetime.date.fromtimestamp(strDate)
+            ageDays = (datetime.date.today() - dt_object).days
+            print('        I Last update:' + str(dt_object) + '  | ' + str(ageDays) + ' days ago')
+            if ageDays > 365:
+                print("        - Wow this is reaaaaly old, more than a year and nothing. think about deleting this bro  ;(")
+            elif ageDays > 180:
+                print("        - I see some spiderwebs, 6 months and you have not worked on this, take a look  ;(")
+            elif ageDays > 90:
+                print("        - Forgot about this? 3 months ago it was important, how about now?  ;(")
 
 except requests.exceptions.RequestException as e: 
     print e
