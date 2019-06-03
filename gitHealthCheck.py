d1237e79e9921c3ad3d15940c5f30ac59ccdf8b2
Pacho: Francisco Nino <francisco.nino@experian.com>
2019-06-03 12:36:06 -0400

    Check pull requests status

--- gitHealthCheck.py
+++ gitHealthCheck.py
@@ -44,38 +44,42 @@ try:
         response = requests.get(args['baseurl'] + 'rest/api/1.0/projects/' + args['project'] + '/repos/' + repo + '/branches', headers=headers, params=(('limit', '100'),('details', 'true'),))
         print('\n repo: *' + repo + '*') 
         response_jsondata = json.loads(response.content, encoding=None)
+        #print(response.content)
 
         branches = response_jsondata['values']
-        #print(branches)
         for branch in branches:
 
             #print(branch['metadata'])
-            #print(branch['metadata']['com.atlassian.bitbucket.server.bitbucket-branch:latest-commit-metadata']['authorTimestamp'])
             strDate = branch['metadata']['com.atlassian.bitbucket.server.bitbucket-branch:latest-commit-metadata']['authorTimestamp']/1000 # remove last 3 zeros from timestamp value
-            #dt_object = datetime.fromtimestamp(strDate)
             dt_object = datetime.date.fromtimestamp(strDate)
             ageDays = (datetime.date.today() - dt_object).days
             print('    branch: *' + branch['displayId'] + '* (last update on ' + str(dt_object) + '  | ' + str(ageDays) + ' days ago)')
 
+            # Review associated Pull Requests status
+            try: 
+                if branch['metadata']['com.atlassian.bitbucket.server.bitbucket-ref-metadata:outgoing-pull-request-metadata']['pullRequest']['state'].upper() == 'MERGED':
+                    print('        Merged branches *MUST* be deleted :rage: ')
+            except KeyError:
+                pass
+
             message = ""
 
-            # Check Branch Name
+            # Check Branch Naming conventions
             if branch['displayId'].upper() == 'MASTER':
                 message = "        + You have a master" 
-                #print("        + You have a master :ok_hand: ")
                 if str(branch['isDefault']) == "True":
                     message+= " and is set as default branch :thumbsup: "
                 else:
                     message+= "but is NOT your default branch :rage: "
             
             elif branch['displayId'].upper() == 'DEVELOPMENT' or branch['displayId'].upper() == 'RELEASE' or branch['displayId'].upper() == 'INTEGRATION':
-                message = "        - Hummm... you shouldn't be using this branch name :broken_heart: "
+                message = "        Hummm... you shouldn't be using this branch name :broken_heart: "
             else:
                 pattern = 'feature/[A-Z]\w+-[0-9]\w+'
                 if re.match(pattern, branch['displayId']):
-                    message += ", Nice branch name, i like it ;)"
+                    message += "        Nice branch name, i like it ;)"
                 else:
-                    message += ", Don't know if I like your branch name that much  :thumbsdown:"
+                    message += "        Don't know if I like your branch name that much  :thumbsdown:"
             
             if ageDays > 365:
                 message += ". about deleting this bro!  :skull:"
